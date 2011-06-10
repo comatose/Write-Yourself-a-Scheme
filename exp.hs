50fd8a7cc5815642ae7d4752b695c075d6ce5490
Boncheol Gu <boncheol.gu@gmail.com>
2011-06-10 15:25:13 +0900

    a little bit Monadic

--- exp.hs
+++ exp.hs
@@ -1,17 +1,53 @@
-data Tree = Term Char | Node Tree Char Tree deriving Show
+import Control.Monad.State
 
-parseNum ::  String -> Maybe (Tree, String)
-parseNum (x:xs)  = Just (Term x, xs)
-parseNum _ = Nothing
+type Number = Int
+type Operator = Char
+data Expr = Term Number | Node Expr Operator Expr deriving Show
 
-parseExp ('(':xs) = Just (Node left op right, rest)
-                where
-                    Just (left, (op:ys)) = parse xs
-                    Just (right, (')':rest)) = parse ys
-parseExp _ = Nothing
+parseNum ::  String -> (Maybe Expr, String)
+parseNum (x:xs)  = (Just $ Term $ read [x], xs)
+parseNum xs = (Nothing, xs)
 
-parse xs = case parseExp xs of
-                Nothing -> parseNum xs
-                x -> x
+parseNum' ::  State String (Maybe Expr)
+parseNum' = state parseNum
 
-test = parse "((0+(1+(2*3)))+4)"
+parseOp :: String -> (Maybe Operator, String)
+parseOp (x:xs) | x `elem` "+*-/" = (Just x, xs)
+               | otherwise = (Nothing, xs)
+
+parseOp' ::  State String (Maybe Operator)
+parseOp' = state parseOp
+
+char :: Char -> State String (Maybe Char)
+char c = do
+            (x:xs) <- get
+            if x == c
+                then put xs >> (return $ Just x)
+                else return Nothing
+
+parseExpr' :: State String (Maybe Expr)
+parseExpr' = do
+               val <- char '('
+               if val == Nothing
+                   then return Nothing
+                   else do
+                       Just left <- parse'
+                       Just op <- parseOp'
+                       Just right <- parse'
+                       char ')'
+                       return $ Just $ Node left op right
+
+parse' :: State String (Maybe Expr)
+parse' = do
+            val <- parseExpr'
+            case val of
+                Nothing -> parseNum'
+                x -> return val
+
+testchar = runState (char '(') "(090)"
+testNum = runState parseNum' "1aaa"
+testOp = runState parseOp' "+iaa"
+testExpr = runState parseExpr' "(1+2)"
+
+test = runState parse' "((0+(1+(2*3)))+4)"
+test2 = runState parse' "4 1123"
